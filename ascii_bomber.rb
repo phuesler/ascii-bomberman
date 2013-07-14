a89daacd10e1b10f4d2aee2479fecafe5520128c
Patrick Huesler <patrick.huesler@gmail.com>
2013-07-14 14:27:43 +0200

    create and readm from input buffer

--- ascii_bomber.rb
+++ ascii_bomber.rb
@@ -2,23 +2,42 @@
 #
 require 'curses'
 
-Curses.noecho # do not show typed keys
-Curses.init_screen
-Curses.stdscr.keypad(true) # enable arrow keys
-Curses.setpos(0, 0)
-#loop do
-  #char = Curses.getch
-  #Curses.addstr(char.to_s)
-#end
-def game_loop
-  loop do
-    Curses.setpos(0, 0)
-    Curses.addstr("render #{Time.now.to_s}")
-    Curses.refresh
-    sleep(0.25)
+def read_keyboard_input(input_buffer)
+  Thread.new do
+    loop do
+      input_buffer << Curses.getch
+    end
+  end
+end
+def init_curses
+  Curses.noecho # do not show typed keys
+  Curses.init_screen
+  Curses.stdscr.keypad(true) # enable arrow keys
+end
+
+def game_loop(input_buffer)
+  Thread.new do
+    loop do
+      Curses.setpos(0, 0)
+      Curses.addstr("render #{Time.now.to_s} #{input_buffer.size}")
+      Curses.refresh
+      sleep(0.25)
+    end
   end
 end
 
-game_loop
-Curses.close_screen
+def run
+  begin
+    init_curses
+    input_buffer = []
+    keyboard = read_keyboard_input(input_buffer)
+    game = game_loop(input_buffer)
+    keyboard.join
+    game.join
+  ensure
+    Curses.close_screen
+    puts 'ENSURE'
+  end
+end
 
+run
